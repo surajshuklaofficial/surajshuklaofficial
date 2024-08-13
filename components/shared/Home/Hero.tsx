62d903940627b194a04780e5dc155b82bcbc3026
surajshukla <surajshukla5604@gmail.com>
2024-08-13 19:20:48 +0530

    fix: ESlint issue

--- components/shared/Home/Hero.tsx
+++ components/shared/Home/Hero.tsx
@@ -1,4 +1,4 @@
-import { ENGINEERING, HERO } from "@/public";
+import { ENGINEERING } from "@/public";
 import Image from "next/image";
 
 export const Hero = () => {
@@ -7,10 +7,10 @@ export const Hero = () => {
       <div className="w-full px-40 flex flex-col md:flex-row items-center justify-center gap-20 -translate-y-1/3">
         <div className="text-center md:text-left">
           <h1 className="text-4xl md:text-6xl font-bold leading-tight">
-            Hi, I'm Suraj Shukla
+            Hi, I&apos;m Suraj Shukla
           </h1>
           <p className="mt-4 text-lg md:text-2xl">
-            I'm pursuing Software Engineering at Delhi Technological University
+            I&apos;m pursuing Software Engineering at Delhi Technological University
           </p>
         </div>
         <div className="mtgit-10 md:mt-0 md:w-1/2">
@@ -24,5 +24,5 @@ export const Hero = () => {
         </div>
       </div>
     </section>
-  );
+  )
 };
